38a33b69236f7deacc239246db7760b7450029b5
Geovana Gonçalo <mariageovana82268@gmail.com>
2026-08-12 23:42:29 -0300

    feat: adicionar item e aba criativa do mod

--- src/main/java/com/gdev13/pusheenslittleworld/PusheensLittleWorld.java
+++ src/main/java/com/gdev13/pusheenslittleworld/PusheensLittleWorld.java
@@ -2,6 +2,7 @@ package com.gdev13.pusheenslittleworld;
 
 import org.slf4j.Logger;
 
+import com.gdev13.pusheenslittleworld.registry.ModCreativeTabs;
 import com.gdev13.pusheenslittleworld.registry.ModItems;
 import com.mojang.logging.LogUtils;
 
@@ -18,6 +19,7 @@ public class PusheensLittleWorld {
 
     public PusheensLittleWorld(IEventBus modEventBus, ModContainer modContainer) {
         ModItems.register(modEventBus);
+        ModCreativeTabs.register(modEventBus);
 
         LOGGER.info("Pusheen's Little World carregado!");
     }
---
+++ src/main/java/com/gdev13/pusheenslittleworld/registry/ModCreativeTabs.java
@@ -0,0 +1,30 @@
+package com.gdev13.pusheenslittleworld.registry;
+
+import com.gdev13.pusheenslittleworld.PusheensLittleWorld;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.core.registries.Registries;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModCreativeTabs {
+
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, PusheensLittleWorld.MODID);
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> PUSHEEN_TAB =
+            CREATIVE_MODE_TABS.register("pusheen_tab", () -> CreativeModeTab.builder()
+                    .title(Component.literal("Pusheen's Little World"))
+                    .icon(() -> new ItemStack(ModItems.PUSHEEN_PLUSH.get()))
+                    .displayItems((parameters, output) -> {
+                        output.accept(ModItems.PUSHEEN_PLUSH.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
