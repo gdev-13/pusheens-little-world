d8e0bf63aaefa28ac70fa0fbdc849e94016da1b5
Geovana Gonçalo <mariageovana82268@gmail.com>
2026-08-21 12:05:29 -0300

    feat: ajustar modelo e exibição de Pusheen Mug

--- src/main/java/com/gdev13/pusheenslittleworld/block/PusheenMugBlock.java
+++ src/main/java/com/gdev13/pusheenslittleworld/block/PusheenMugBlock.java
@@ -1,20 +1,29 @@
 package com.gdev13.pusheenslittleworld.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class PusheenMugBlock extends Block {
+	
+	public static final EnumProperty<Direction> FACING = 
+			BlockStateProperties.HORIZONTAL_FACING;
 
     private static final VoxelShape SHAPE = Shapes.or(
             Block.box(4, 0, 4, 12, 8, 12)
     );
 
-    public PusheenMugBlock(Properties properties) {
+    public PusheenMugBlock(BlockBehaviour.Properties properties) {
         super(properties);
     }
 
@@ -27,4 +36,15 @@ public class PusheenMugBlock extends Block {
 
         return SHAPE;
     }
+    
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+    
+    @Override
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+	    return this.defaultBlockState()
+	            .setValue(FACING, context.getHorizontalDirection());
+	}
 }
